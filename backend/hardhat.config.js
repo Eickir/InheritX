0dd29c41250e79d4cb527fa305fedb4b10d64f4f
Richard Lavoura <richardlavoura@mbp-de-richard.home>
2025-03-25 20:17:32 +0100

    feat: install uniswapV2 and configure hardhat.config.js to fork the mainnet

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,6 +1,31 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  solidity: {
+    compilers: [
+      {version: "0.8.28"}, // Newest contract
+      {
+        version: "0.4.17", // Pour ton contrat USDT
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 200
+            },
+            evmVersion: "byzantium"  // 🔥 Version EVM adaptée à Solidity 0.4.17
+        }
+    }
+    ]
+  }, 
+  defaultNetwork: "hardhat", 
+  networks: {
+    hardhat: {
+      forking: {
+        enabled: true, 
+        url: process.env.MAINNET_RPC_URL,
+        blockNumber: 22123608
+      }
+    }
+  }
 };
