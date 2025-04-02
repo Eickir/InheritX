a8f28c55aa9262eaf7f82f15a3deb41093acc24e
Richard Lavoura <richardlavoura@mbp-de-richard.home>
2025-04-02 02:02:51 +0200

    feat: add new routes for testator

--- frontend/app/testator/page.js
+++ frontend/app/testator/page.js
@@ -1,7 +1,7 @@
 "use client";
 import { useAccount } from "wagmi";
 import NotConnected from "@/components/shared/NotConnected";
-import DashboardTestament from "@/components/shared/testator_dashboard/DashboardTestament";
+import DashboardTestament from "@/components/shared/testator/dashboard/DashboardTestament";
 
 export default function Testator() {
 
