7c1ae4c420d7bf4a7fa3cc5e67cf6342b2c9f609
Richard Lavoura <richardlavoura@macbook-pro-de-richard.home>
2025-04-06 13:55:55 +0200

    feat: condition config hardhat to fetch sepolia only if it found a sepolia rpc url

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,32 +1,40 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL;
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+
+const networks = {
+  hardhat: {
+    forking: {
+      url: MAINNET_RPC_URL,
+      enabled: true,
+      blockNumber: 22123608,
+    },
+  },
+};
+
+// Ajoute la configuration sepolia uniquement si les variables sont définies et valides
+if (SEPOLIA_RPC_URL && PRIVATE_KEY && PRIVATE_KEY.length === 64) {
+  networks.sepolia = {
+    url: SEPOLIA_RPC_URL,
+    chainId: 11155111,
+    accounts: [`0x${PRIVATE_KEY}`],
+  };
+}
+
 module.exports = {
   solidity: {
-    compilers: [
-      {version: "0.8.28"}, 
-    ]
-  }, 
-  defaultNetwork: "hardhat", 
-  networks: {
-    hardhat: {
-      forking: {
-        url: process.env.MAINNET_RPC_URL,
-        enabled: true, 
-        blockNumber: 22123608
-      }
-    }, 
-    sepolia: {
-      url: process.env.SEPOLIA_RPC_URL,
-      chainId: 11155111,
-      accounts: [`0x${process.env.PRIVATE_KEY}`]
-    }
+    compilers: [{ version: "0.8.28" }],
   },
+  defaultNetwork: "hardhat",
+  networks,
   gasReporter: {
     enabled: true,
-  }, 
+  },
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
-  }
+    apiKey: ETHERSCAN_API_KEY,
+  },
 };
